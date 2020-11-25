377437f99c54858a60a53ac96e732a2b406c7856
Nigar Sultana <74977812+Nigarshipa@users.noreply.github.com>
2020-11-25 20:14:29 +0600

    Add files via upload
    uri problem 1173 in c++

---
+++ 1173.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,a[10],i;
+    cin>>x;
+
+    for(int i=0; i<10; i++)
+    {
+
+
+        cout<<"N"<<"["<<i<<"]"<<" "<<"="<<" "<<x<<endl;
+
+        x=x*2;
+
+    }
+
+
+
+
+
+
+}
